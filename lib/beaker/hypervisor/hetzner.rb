752f6b9124d8f3b62e43a8ee85673222e7041298
Tim Meusel <tim@bastelfreak.de>
2022-03-03 19:05:15 +0100

    Implement image verification

--- lib/beaker/hypervisor/hetzner.rb
+++ lib/beaker/hypervisor/hetzner.rb
@@ -14,20 +14,50 @@ module Beaker
       raise 'You need to pass a token as HCLOUD_TOKEN environment variable' unless ENV['HCLOUD_TOKEN']
 
       @client = Hcloud::Client.new(token: ENV['HCLOUD_TOKEN'])
+      @images = images
     end
 
     def provision
-      @logger.notify 'Provisioning docker'
+      @logger.notify "Provisioning #{@hosts.count} hcloud instance(s)"
       @hosts.each do |host|
+        os, rel, _arch = host.platform.split('-')
+        image = "#{os}-#{rel}"
         @logger.notify "provisioning #{host.name}"
+        puts '=============='
+        puts "Image is #{host.platform}"
+        puts '=============='
+        raise "image #{image} is not supported at hcloud. supported are: #{images}" unless image_supported(image)
+
+        # TODO: figure the domain out. does that come from beaker-hostgenerator
+        # TODO use ssh keys?
+        _action, server, pw = @client.servers.create(name: 'moo5', server_type: 'cx11', image: image)
+
+        binding.pry
       end
     end
 
     def cleanup
-      @logger.notify 'Cleaning up docker'
+      @logger.notify "Cleaning up #{@hosts.count} hcloud instance(s)"
       @hosts.each do |host|
         # @logger.debug("stop VM #{container.id}")
       end
+      # instead of drestroying all, we should save the ids somewhere and only destroy those
+      @client.servers.all.each(&:destroy)
+    end
+
+    private
+
+    # array of images that hetzner has
+    # ["centos-7", "ubuntu-18.04", "debian-10", "ubuntu-20.04", "debian-11", "centos-stream-8", "rocky-8", "fedora-35", "centos-stream-9"]
+    def images
+      # TODO: migrate to os_favor/os_version
+      # https://docs.hetzner.cloud/#images-get-all-images
+      @client.images.select { |image| image.type == 'system' && image.status == 'available' }.map(&:name)
+    end
+
+    # takes the string from beaker and returns true if hetzner supports it
+    def image_supported(image)
+      @images.include?(image)
     end
   end
 end
